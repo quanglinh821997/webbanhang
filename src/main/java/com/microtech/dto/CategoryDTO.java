11afd45d7e05a9bb016b60401c9adb12d4718100
Quang Linh <quanglinh821997@gmail.com>
2022-09-15 14:35:02 +0700

    lan 1

---
+++ src/main/java/com/microtech/dto/CategoryDTO.java
@@ -0,0 +1,13 @@
+package com.microtech.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CategoryDTO {
+    private int id;
+    private String name;
+}
